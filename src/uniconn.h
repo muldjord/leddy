fe068c3ceb94fd8baa4d67c8245a0c0a08fbf58c
Lars Muldjord <muldjordlars@gmail.com>
2020-08-01 20:04:28 +0200

    Moved from spiconn.h

---
+++ src/uniconn.h
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/***************************************************************************
+ *            uniconn.h
+ *
+ *  Fri Jul 24 12:00:00 CEST 2020
+ *  Copyright 2020 Lars Muldjord
+ *  muldjordlars@gmail.com
+ ****************************************************************************/
+/*
+ *  This file is part of Leddy.
+ *
+ *  Leddy is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Leddy is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Leddy; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ */
+
+#ifndef _UNICONN_H
+#define _UNICONN_H
+
+#include <stdint.h>
+
+#include <QObject>
+#include <QImage>
+#include <QTimer>
+#include <QEventLoop>
+
+class UniConn : public QObject
+{
+  Q_OBJECT
+
+public:
+  UniConn(QByteArray device, uint32_t speed, uint8_t mode, uint8_t bits,
+	  int brightness = 50, int rotation = 0);
+  ~UniConn();
+  bool init();
+  bool refresh();
+  void clear(const QColor color = QColor(Qt::black));
+  void setFromImage(const QImage image);
+  void setPixel(const int x, const int y, const QColor color);
+  void drawText(const int x, const int y,
+		const QString text, const int spacing = 0, const QColor color = QColor(Qt::white));
+  
+private:
+  QTimer limitTimer;
+  QEventLoop limiter;
+  QImage buffer = QImage(16, 16, QImage::Format_RGB888);
+  QByteArray device = "/dev/spidev0.0";
+  uint8_t bits = 8;
+  uint8_t mode = 0;
+  uint32_t speed = 9000000;
+  int rotation = 0;
+  int brightness = 50;
+  
+  bool isOpen = false;
+  int fd;
+};
+
+#endif // _UNICONN_H
