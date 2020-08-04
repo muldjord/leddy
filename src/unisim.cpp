26810d5f7c2c12fdeb79cd90e70bed9ff9aa861c
Lars <muldjordlars@gmail.com>
2020-08-04 10:43:55 +0200

    Unicorn Hat HD simulator

---
+++ src/unisim.cpp
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/***************************************************************************
+ *            unisim.cpp
+ *
+ *  Fri Jul 24 12:00:00 CEST 2020
+ *  Copyright 2020 Lars Muldjord
+ *  muldjordlars@gmail.com
+ ****************************************************************************/
+/*
+ *  This file is part of Leddy.
+ *
+ *  Leddy is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Leddy is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Leddy; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ */
+
+#include "unisim.h"
+
+UniSim::UniSim()
+{
+}
+
+UniSim::~UniSim(){
+}
+
+void UniSim::setScene(const QImage &scene)
+{
+  setPixmap(QPixmap::fromImage(scene));
+}
